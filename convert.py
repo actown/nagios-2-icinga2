834a6f7c5b605e616f5bfd34b69b986ef1be0c09
Derrick Dymock <ddymock@flightstats.com>
2016-05-24 13:38:27 -0700

    Add zk_node check support.

--- convert.py
+++ convert.py
@@ -15,7 +15,12 @@ resources = []
 
 def parse_graphite(check_cmd):
     check = check_cmd.split('!')
-    return {'command': check[0].lstrip('check_'), 'period': check[1], 'warn': check[2], 'crit': check[3], 'target': check[4]}
+    return {'command': check[0].lstrip('check_'), 'period': check[1], 'warn': check[2], 'crit': check[3], 'target': check[4].replace('"', '\\"')}
+
+
+def parse_zk_node(check_cmd):
+    check = check_cmd.split('!')
+    return {'command': check[0].lstrip('check_'), 'key': check[1], 'warn': check[2], 'crit': check[3]}
 
 for line in nagios_file:
     if line[0] is '#':
@@ -47,4 +52,18 @@ for resource in resources:
     assign where host.name == \"%s\"
 }
 """ % (resource['service_name'].replace(' ', '_').lower(), resource['service_name'], graphite['target'], graphite['period'], graphite['warn'], graphite['crit'], graphite['command'], resource['host_name'])
+    if 'check_zk_node' in resource['check_command']:
+        zk_node = parse_zk_node(resource['check_command'])
+        text = """apply Service \"%s\" {
+    display_name = \"%s\"
+
+    vars.zookeeper_key = \"%s\"
+    vars.zookeeper_warn = \"%s\"
+    vars.zookeeper_crit = \"%s\"
+
+    check_command = "zookeeper_node"
+
+    assign where host.name == \"%s\"
+}
+""" % (resource['service_name'].replace(' ', '_').lower(), resource['service_name'], zk_node['key'],  zk_node['warn'], zk_node['crit'], resource['host_name'])
     icinga_file.write(text)
